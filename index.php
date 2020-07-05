58ab47ebd4880a3206bc2d2fbf669a38aa05bf67
Chigozie peter <ujahchigozie@gmail.com>
2020-07-05 20:53:00 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript">
+function googleTranslateElementInit2() {
+	new google.translate.TranslateElement({pageLanguage: 'en',autoDisplay: false}, 'google_translate_element2');
+}
+</script>
+<script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2"></script>
+</head>
+    <body >
+		
+		<div class="">
+			<h2>We have a long history of service in the Bay Area</h2>
+
+			<p>We were one of the first credit unions that operate world wide, founded in 1932 as City &amp; County Employees' Credit Union. 
+		Membership is now open to anyone who lives, works, or attends school in 
+		Alameda, Contra Costa, San Joaquin, Solano, Stanislaus, or Kings counties in California. 
+		We believe in banking locally and hope you will too. </p>
+		</div>
+		<div class="button"> <?php include_once(__DIR__ . '/plugin/translatore-ui.php');?></div>
+
+		<?php include_once(__DIR__ . '/plugin/translatore-plugin.php');?>
+	</body>
+</html>
---
+++ plugin/translatore-ui.php
@@ -0,0 +1,37 @@
+<div style="position:absolute;" id="gtranslate_wrapper">
+	<div class="switcher notranslate">
+	<div class="selected">
+	<a href="#" id="return false;" class=""><img data-gt-lazy-src="image/flags/16/en.png" alt="fr" src="image/flags/16/en.png" width="16" height="16"> English</a>
+	</div>
+		<div class="option" style="display: none;">
+			<a href="#" onclick="doGTranslate('en|en');$('div.switcher div.selected a').html($(this).html());return false;" title="English" class="nturl selected"><img data-gt-lazy-src="image/flags/16/en.png" alt="en" src="image/flags/16/en.png" width="16" height="16"> English</a>
+
+			<a href="#" onclick="doGTranslate('en|ar');$('div.switcher div.selected a').html($(this).html());return false;" title="Arabic" class="nturl"><img data-gt-lazy-src="image/flags/16/ar.png" alt="ar" src="image/flags/16/ar.png" width="16" height="16"> Arabic</a>
+
+			<a href="#" onclick="doGTranslate('en|fr');$('div.switcher div.selected a').html($(this).html());return false;" title="French" class="nturl"><img data-gt-lazy-src="image/flags/16/fr.png" alt="fr" src="image/flags/16/fr.png" width="16" height="16"> French</a>
+
+			<a href="#" onclick="doGTranslate('en|de');$('div.switcher div.selected a').html($(this).html());return false;" title="German" class="nturl"><img data-gt-lazy-src="image/flags/16/de.png" alt="de" src="image/flags/16/de.png" width="16" height="16"> German</a>
+
+			<a href="#" onclick="doGTranslate('en|zh-CN');$('div.switcher div.selected a').html($(this).html());return false;" title="Chinese (Simplified)" class="nturl"><img data-gt-lazy-src="image/flags/16/zh-CN.png" alt="zh-CN" src="image/flags/16/zh-CN.png" width="16" height="16"> Chinese (Simplified)</a>
+
+			<a href="#" onclick="doGTranslate('en|it');$('div.switcher div.selected a').html($(this).html());return false;" title="Italian" class="nturl"><img data-gt-lazy-src="image/flags/16/it.png" alt="it" src="image/flags/16/it.png" width="16" height="16"> Italian</a>
+
+			<a href="#" onclick="doGTranslate('en|pt');$('div.switcher div.selected a').html($(this).html());return false;" title="Portuguese" class="nturl"><img data-gt-lazy-src="image/flags/16/pt.png" alt="pt" src="image/flags/16/pt.png" width="16" height="16"> Portuguese</a>
+
+			<a href="#" onclick="doGTranslate('en|es');$('div.switcher div.selected a').html($(this).html());return false;" title="Spanish" class="nturl"><img data-gt-lazy-src="image/flags/16/es.png" alt="es" src="image/flags/16/es.png" width="16" height="16"> Spanish</a>
+
+			<a href="#" onclick="doGTranslate('en|ms');$('div.switcher div.selected a').html($(this).html());return false;" title="Malay" class="nturl"><img data-gt-lazy-src="image/flags/16/ms.png" alt="ms" src="image/flags/16/ms.png" width="16" height="16"> Malay</a>
+
+			<a href="#" onclick="doGTranslate('en|ru');$('div.switcher div.selected a').html($(this).html());return false;" title="Russian" class="nturl"><img data-gt-lazy-src="image/flags/16/ru.png" alt="ru" src="image/flags/16/ru.png" width="16" height="16"> Russian</a>
+
+			<a href="#" onclick="doGTranslate('en|nl');$('div.switcher div.selected a').html($(this).html());return false;" title="Dutch" class="nturl"><img data-gt-lazy-src="image/flags/16/nl.png" alt="nl" src="image/flags/16/nl.png" width="16" height="16"> Dutch</a>
+
+			<a href="#" onclick="doGTranslate('en|id');$('div.switcher div.selected a').html($(this).html());return false;" title="Indonesian" class="nturl"><img data-gt-lazy-src="image/flags/16/id.png" alt="id" src="image/flags/16/id.png" width="16" height="16"> Indonesian</a>
+
+			<a href="#" onclick="doGTranslate('en|ja');$('div.switcher div.selected a').html($(this).html());return false;" title="Japanese" class="nturl"><img data-gt-lazy-src="image/flags/16/ja.png" alt="ja" src="image/flags/16/ja.png" width="16" height="16"> Japanese</a>
+
+			<a href="#" onclick="doGTranslate('en|ko');$('div.switcher div.selected a').html($(this).html());return false;" title="Korean" class="nturl"><img data-gt-lazy-src="image/flags/16/ko.png" alt="ko" src="image/flags/16/ko.png" width="16" height="16"> Korean</a>
+		</div>
+	</div>
+	<div id="google_translate_element2"></div>
+</div>
